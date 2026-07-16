60c04eb1b459a70d475d7e679dc6ed954885e8f4
Animesh Daiman <110604520+AnimeshDaiman7@users.noreply.github.com>
2026-07-16 20:33:39 +0530

    Create solution.py

---
+++ Day-48/solution.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+class Solution:
+    def subarraySum(self, nums: List[int], k: int) -> int:
+
+        prefix = defaultdict(int)
+        prefix[0] = 1
+
+        curr_sum = 0
+        count = 0
+
+        for num in nums:
+            curr_sum += num
+
+            count += prefix[curr_sum - k]
+
+            prefix[curr_sum] += 1
+
+        return count
