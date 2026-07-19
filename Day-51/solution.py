d76cd2ff48bb58b4e0a3553799e7597abc9d407b
Animesh Daiman <110604520+AnimeshDaiman7@users.noreply.github.com>
2026-07-19 22:49:57 +0530

    Create solution.py

---
+++ Day-51/solution.py
@@ -0,0 +1,37 @@
+from typing import List
+
+class Solution:
+    def pacificAtlantic(self, heights: List[List[int]]) -> List[List[int]]:
+        if not heights or not heights[0]:
+            return []
+
+        rows, cols = len(heights), len(heights[0])
+
+        pacific = set()
+        atlantic = set()
+
+        directions = [(1,0), (-1,0), (0,1), (0,-1)]
+
+        def dfs(r, c, visited):
+            visited.add((r, c))
+
+            for dr, dc in directions:
+                nr, nc = r + dr, c + dc
+
+                if (
+                    0 <= nr < rows and
+                    0 <= nc < cols and
+                    (nr, nc) not in visited and
+                    heights[nr][nc] >= heights[r][c]
+                ):
+                    dfs(nr, nc, visited)
+
+        for r in range(rows):
+            dfs(r, 0, pacific)
+            dfs(r, cols - 1, atlantic)
+
+        for c in range(cols):
+            dfs(0, c, pacific)
+            dfs(rows - 1, c, atlantic)
+
+        return [[r, c] for (r, c) in pacific & atlantic]
