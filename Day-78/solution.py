7dd9f3edaaab155d1190bb21e4b1e651d1218d9b
Animesh Daiman <110604520+AnimeshDaiman7@users.noreply.github.com>
2026-08-17 19:57:17 +0530

    Create solution.py

---
+++ Day-78/solution.py
@@ -0,0 +1,43 @@
+class Solution:
+    def isScramble(self, s1: str, s2: str) -> bool:
+
+        memo = {}
+
+        def solve(a, b):
+            if a == b:
+                return True
+
+            key = (a, b)
+
+            if key in memo:
+                return memo[key]
+
+            if len(a) != len(b):
+                return False
+
+            if sorted(a) != sorted(b):
+                memo[key] = False
+                return False
+
+            n = len(a)
+
+            for i in range(1, n):
+
+                # No swap
+                if (solve(a[:i], b[:i]) and
+                    solve(a[i:], b[i:])):
+
+                    memo[key] = True
+                    return True
+
+                # Swap
+                if (solve(a[:i], b[n-i:]) and
+                    solve(a[i:], b[:n-i])):
+
+                    memo[key] = True
+                    return True
+
+            memo[key] = False
+            return False
+
+        return solve(s1, s2)
